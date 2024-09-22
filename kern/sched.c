1b139095cf17455e4f4094961b9a4bd75d760a71
Daniel Masud <danielbmasud@gmail.com>
2024-09-22 17:11:56 -0500

    Adding vmxon when running a guest env

--- kern/sched.c
+++ kern/sched.c
@@ -48,12 +48,18 @@ sched_yield(void)
 		// If this environment is runnable, run it.
 		if (envs[k].env_status == ENV_RUNNABLE) {
             /* Your code here */
+			if (envs[k].env_type == ENV_TYPE_GUEST) {
+				vmxon();
+			}
 			env_run(&envs[k]);
 		}
 	}
 
 	if (curenv && curenv->env_status == ENV_RUNNING) {
         /* Your code here */
+		if (curenv->env_type == ENV_TYPE_GUEST) {
+			vmxon();
+		}
 		env_run(curenv);
 	}
 
