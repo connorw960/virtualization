ceb962bcfe94987eca0a63423e7ca984c8b434f4
Connor Walker <connorw960@utexas.edu>
2024-09-30 23:31:35 +0000

    Fixed vmxon

--- kern/sched.c
+++ kern/sched.c
@@ -47,19 +47,17 @@ sched_yield(void)
 		k = (j + i) % NENV;
 		// If this environment is runnable, run it.
 		if (envs[k].env_status == ENV_RUNNABLE) {
-            if (envs[k].env_type == ENV_TYPE_GUEST) 
-			{
-				vmxon();
-			}
+			#ifndef VMM_GUEST
+			vmxon();
+			#endif
 			env_run(&envs[k]);
 		}
 	}
 
 	if (curenv && curenv->env_status == ENV_RUNNING) {
-        if (curenv->env_type == ENV_TYPE_GUEST) 
-		{
-			vmxon();
-		}
+        #ifndef VMM_GUEST
+		vmxon();
+		#endif
 		env_run(curenv);
 	}
 
